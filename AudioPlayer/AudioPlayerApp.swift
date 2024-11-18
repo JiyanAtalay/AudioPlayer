32ddf548a40c549d77ab34cabb7c97a9560a821a
JiyanAtalay <jyntly23@gmail.com>
2024-11-18 21:43:43 +0300

    Initial Commit

---
+++ AudioPlayer/AudioPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioPlayerApp.swift
+//  AudioPlayer
+//
+//  Created by Mehmet Jiyan Atalay on 18.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct AudioPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
